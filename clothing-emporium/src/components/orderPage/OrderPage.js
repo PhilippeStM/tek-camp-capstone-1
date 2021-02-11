90d293b0ccc644c5af453ecc9ffd23908659b318
phil-stm <philippestm@yahoo.com>
2021-02-11 13:33:13 -0800

    continued working on order page. added shirt map.

--- clothing-emporium/src/components/orderPage/OrderPage.js
+++ clothing-emporium/src/components/orderPage/OrderPage.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import NavBar from '../navBar/NavBar';
+import shirts from '../../server/ShirtInventory.json';
+import dresses from '../../server/DressInventory.json';
+import pants from '../../server/PantsInventory.json';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import styles from '../../styles/OrderPage.css';
 
 const OrderPage = () => {
-    return(
-        <div>
+
+    const shirtInventory = shirts;
+    console.log(shirtInventory);
+
+    const dressInventory = dresses;
+    console.log(dressInventory);
+
+    const pantsInventory = pants;
+    console.log(pantsInventory);
+
+    return (
+        <center>
             <NavBar />
             <h3>This is the order page!</h3>
-        </div>
+            <CardDeck>
+                {shirtInventory.shirts.map(data => (
+                        <Card className="card">
+                            <Card.Img src={data.productImage}></Card.Img>
+                            <Card.Title>{data.productName}</Card.Title>
+                            <Card.Body>
+                                <Card.Text>
+                                    <p>{data.description}</p>
+                                    <p>${data.price}</p>
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted">ADD TO CART </small>
+                                <small className="text-muted">({data.quantity} in stock)</small>
+                            </Card.Footer>
+                        </Card>
+                ))}
+            </CardDeck>
+        </center>
     )
 }
 
