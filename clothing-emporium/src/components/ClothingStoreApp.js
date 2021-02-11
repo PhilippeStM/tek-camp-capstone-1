08b79be82ffdc5b969e48a8a89dc326066990277
phil-stm <philippestm@yahoo.com>
2021-02-10 16:18:44 -0800

    Working on skeleton of react app. Formatting components.

---
+++ clothing-emporium/src/components/ClothingStoreApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './homePage/HomePage';
+
+const ClothingStoreApp = () => {
+    
+    return(
+        <div>
+            <HomePage />
+        </div>
+    )
+}
+
+export default ClothingStoreApp;
---
+++ clothing-emporium/src/components/homePage/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from '../navBar/NavBar';
+
+const HomePage = () => {
+
+
+    return(
+
+        <div>
+            <h2>This is the Home Page!</h2>
+            <NavBar />
+        </div>
+
+    )
+}
+
+export default HomePage;
